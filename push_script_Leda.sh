9456a8cd81e123c8206dc7a8e3a78e8b3cc52c11
123210298 LUKAS SOARES DO NASCIMENTO <lukas.soares.nascimento@lcc2-53.lcc.ufcg.edu.br>
2024-12-03 14:56:53 -0300

    Commit automático: alterações

--- push_script_Leda.sh
+++ push_script_Leda.sh
@@ -2,17 +2,25 @@
 
 # Carregar as credenciais a partir de variáveis de ambiente
 USERNAME=${GITHUB_USERNAME}
-PASSWORD=${GITHUB_PASSWORD}
+TOKEN=${GITHUB_TOKEN}  # Usando TOKEN em vez de PASSWORD
 
-REPO_URL="https://$USERNAME:$PASSWORD@github.com/LukasNasciment0/scriptsGit"
+# Definir URL do repositório com autenticação via token
+REPO_URL="https://$USERNAME:$TOKEN@github.com/LukasNasciment0/scriptsGit"
+
 # Verifica se há alterações a serem adicionadas
-if [[ -n $(git status -s) ]]; then
+if [[ -n $(git status --porcelain) ]]; then
     echo "Adicionando alterações..."
     git add .
 
-    
+    # Verificar se o argumento de commit foi fornecido
+    if [[ -z "$1" ]]; then
+        echo "Nenhuma mensagem de commit fornecida. Usando mensagem padrão."
+        COMMIT_MSG="Commit automático: alterações"
+    else
+        COMMIT_MSG="$1"
+    fi
 
-    git commit -m "$1"
+    git commit -m "$COMMIT_MSG"
 else
     echo "Nenhuma alteração para adicionar."
 fi
