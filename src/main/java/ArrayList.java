d633224b38e350c6dd15fb9923714f0bd1ba2d5a
123210298 LUKAS SOARES DO NASCIMENTO <lukas.soares.nascimento@lcc1-36.lcc.ufcg.edu.br>
2025-02-20 09:07:21 -0300

    Usando arrays

--- src/main/java/ArrayList.java
+++ src/main/java/ArrayList.java
@@ -1,31 +1,34 @@
+import java.util.ArrayList;
 
 public class ArrayList {
 
-    private Integer[] array;
-    /**
-     * indice do ultimo elemento
-     */
-    private Integer last; 
+   private ArrayList[] array;
    
-    public ArrayList(int capacidadeInicial) {
-        array = new Integer[capacidadeInicial];
-        last = -1;
-    }
+   /**
+    * indice do ultimo elemento
+    */
+   private Integer last; 
+   
+   public ArrayList(int capacidadeInicial) {
+      array = new ArrayList[capacidadeInicial];
+      last = -1;
+   }
 
-    public boolean isEmpty() {
-        return array.length == 0;
-    }
+   public boolean isEmpty() {
+      return size() == 0;
+   }
 
    private boolean isFull(){
       return this.size() == this.array.length;
    }
       
    public void addFirst(int valor) {
-        //if ()
-        
-        //resize();
-        //shiftRight();
-        array[0] = valor;
+      if (isFull())
+         resize(array.length * 2);
+      
+      
+      shiftRight(array, last);
+      array[0] = valor;
     }
 
    public void addLast(int valor) {
@@ -37,108 +40,129 @@ public class ArrayList {
 
    // adiciona um valor no índice passado como parâmetro
    public void add(int index, int valor) {
-      
+      if (isFull())
+        resize(array.length * 2);
    }
 
     
-    // você vai precisar desse método quando tentar adicionar e a fila já estiver cheia
-    private void resize(int novaCapacidade) {
-      Integer[] arrayResized = new Integer[novaCapacidade];
-       //System.arraycopy(array, 0, arrayResized, 0, this.array.length);
-      for (int i = 0; i < this.array.length; i++) 
-           arrayResized[i] = array[i];
+   // você vai precisar desse método quando tentar adicionar e a fila já estiver cheia
+   private void resize(int novaCapacidade) {
+   Integer[] arrayResized = new Integer[novaCapacidade];
+      //System.arraycopy(array, 0, arrayResized, 0, this.array.length);
+   for (int i = 0; i < this.array.length; i++) 
+         arrayResized[i] = array[i];
 
-      array = arrayResized;
-    }
+   array = arrayResized;
+   }
 
-    public Integer[] assegureCapacidade(int capacidadePretendida) {
-        return new Integer[capacidadePretendida];
-    }
+   public Integer[] assegureCapacidade(int capacidadePretendida) {
+      return new Integer[capacidadePretendida];
+   }
+t
+   public int getFirst() {
+      return array[0];
+   }
 
-    public int getFirst() {
-        return array[0];
-    }
+   public int getLast() {
+      return this.array[this.last];
+   }
 
-    public int getLast() {
-        return this.array[this.last];
-    }
+   // retorna o elemento no índice passado como parâmetro
+   // deve lançar IndexOutOfBoundsException se o índice não for válido.
+   public int get(int index) {
+      if (index > last || index < 0)  
+         throw new IndexOutOfBoundsException("Índice inválido!");
+      return array[index];
+   }
 
-    // retorna o elemento no índice passado como parâmetro
-    // deve lançar IndexOutOfBoundsException se o índice não for válido.
-    public int get(int index) {
-        // TODO
-        return -1;
-    }
-
-    // deve lançar exceção caso a fila esteja vazia.
-    public int removeFirst() {
-        // TODO 
-        return 0;
-    }
+   // deve lançar exceção caso a fila esteja vazia.
+   public int removeFirst() {
+      if (isEmpty())
+         throw new ArrayIndexOutOfBoundsException("Fila vazia");
+      return 0;
+   }
 
     // deve lançar exceção caso a fila esteja vazia.
    public void removeLast() {
       if (isEmpty())
          throw new ArrayIndexOutOfBoundsException("Fila vazia");
       
-      this.last--;
-      
-        
-    }
+      this.last--;       
+   }
 
-    // remove o valor no índice passado como parâmetro. 
-    // lançar exceção se o índice não for válido.
-    public void remove(int index) {
-        if (isEmpty())
-            throw new ArrayIndexOutOfBoundsException("Fila vazia");
-        
-        //shifLeft(array, index);
-        last--;
-    }
+   // remove o valor no índice passado como parâmetro. 
+   // lançar exceção se o índice não for válido.
+   public void remove(int index) {
+      if (isEmpty())
+         throw new ArrayIndexOutOfBoundsException("Fila vazia");
+      
+      shifLeft(array, index);
+      last--;
+   }
 
-    // remove a primeira ocorrência do elemento cujo valor foi passado como parâmetro.
-    // se não encontrar, não faça nada.
-    public void removeByValue(int value) {
-      
-      
-      for (int i = 0; i < size(); i++) {
+   // remove a primeira ocorrência do elemento cujo valor foi passado como parâmetro.
+   // se não encontrar, não faça nada.
+   public void removeByValue(int value) {
+      for (int i = 0; i < size(); i++)
          if (array[i] == value){
-            remove(i);
-            break;
+               remove(i);
+               break;
          }
-         
-      }
-    }
+   }
 
-    // retorna o índice da primeira ocorrência do valor passado como parâmetro.
-    public int indexOf(int value) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == value)
-                return i;
-        }
-        return -1;
-    }
-   
-    // Deve retornar a posição da última ocorrência do elemento passado como parâmetro. 
-    public int lastIndexOf(int valor) {
-        return -1;
-    }
+   // retorna o índice da primeira ocorrência do valor passado como parâmetro.
+   public int indexOf(int value) {
+      for (int i = 0; i < array.length; i++)
+         if (array[i] == value)
+               return i;
+      return -1;
+   }
+
+   // Deve retornar a posição da última ocorrência do elemento passado como parâmetro. 
+   public int lastIndexOf(int valor) {
+      return -1;
+   }
     
-    @Override
-    // deve retornar uma string representando a lista. 
-    public String toString() {
+   @Override
+   // deve retornar uma string representando a lista. 
+   public String toString() {
       String output = "";
       for (Object elem : this.array)
-            output += " ";
+            output += elem + ", ";
 
-      return output;
-    }
-    
-    public int size() {
-        int tam = 0;
-        
-        for (int i = 0; i < last; i++)
-           tam++;
-        return tam;
-    }
+      return output.substring(0, output.length() -2);
+   }
+   
+   public int size() {
+      int tam = 0;
+      
+      for (int i = 0; i < last; i++)
+         tam++;
+      return tam;
+   }
+
+   /**
+    * Empurra os elementos do array para a posição do paramentro
+    * @param array
+    * @param index
+    */
+   private void shifLeft(Integer[] array, int index) {
+      for (int i = index; i < last - 1; i++)
+         array[i] = array[i+1];
+      
+      last--;
+
+   }
+
+   /**
+    * Empurra os elementos do array para a direita a partir do index indicado
+    * @param array
+    * @param last
+    */
+   private void shiftRight(Integer[] array, Integer index) {
+      for (int i = last; i > index; i++)
+         array[i + 1] = array[i];
+      
+      last++;
+   }
 }
